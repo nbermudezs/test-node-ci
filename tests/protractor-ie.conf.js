fd0f8e7658d062a65099296e77f6afb4933728d3
Néstor Bermúdez <nestor.bermudezs@gmail.com>
2015-05-24 19:22:01 -0600

    Add run to ie e2e to package.json

--- tests/protractor-ie.conf.js
+++ tests/protractor-ie.conf.js
@@ -1,15 +1,38 @@
 var config = {
+	allScriptsTimeout: 10000,
+
+  specs: ['e2e/*.js'],
+
   capabilities: {
     'browserName': 'internet explorer',
     'platform': 'ANY',
     'version': '11'
+  },
+
+  baseUrl: 'http://localhost:3000',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 60000,
+    isVerbose: true,
+    showTiming: true,
+    includeStackTrace: true
+  },
+
+  onPrepare: function() {
+    browser.manage().window().setSize(1600, 1000);
+    browser.ignoreSynchronization = true;
+    require('./utils/waitReady.js');
+    require('./utils/hasClass.js');
   }
 };
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.capabilities: {
+  config.capabilities = {
     'browserName': 'internet explorer',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
