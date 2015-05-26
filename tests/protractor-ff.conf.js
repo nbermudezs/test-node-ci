a781b26e0b82a21084c0ffba39e3f4b79b099537
Néstor Bermúdez <nestor.bermudezs@gmail.com>
2015-05-25 19:54:43 -0600

    fix wrong browserName

--- tests/protractor-ff.conf.js
+++ tests/protractor-ff.conf.js
@@ -7,12 +7,10 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
-    'browserName': 'internet explorer',
+    'browserName': 'firefox',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
-    'platform': 'ANY',
-    'version': '11',
-    'name': 'E2E On IE'
+    'name': 'E2E On Firefox'
   };
 }
 
